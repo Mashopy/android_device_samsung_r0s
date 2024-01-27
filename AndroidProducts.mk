10c30bab3b4e4b96ba425ddfed31ba1136f5d6fb
Mashopy <eliasgheeraert@gmail.com>
2024-01-27 10:30:21 +0100

    r0s: Initial device tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_r0sxxx.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_r0sxxx-user \
+    lineage_r0sxxx-userdebug \
+    lineage_r0sxxx-eng
---
+++ device.mk
@@ -0,0 +1,55 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Enforce generic ramdisk allow list
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+
+# API
+BOARD_SHIPPING_API_LEVEL := 31
+PRODUCT_SHIPPING_API_LEVEL := 31
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
+# Characteristics
+PRODUCT_CHARACTERISTICS := nosdcard
+
+# Firmware
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/firmware/fts2ba61y_r0.bin:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/firmware/tsp_stm/fts2ba61y_r0.bin \
+    $(LOCAL_PATH)/configs/firmware/fts2ba61y_r0.bin:$(TARGET_RECOVERY_VENDOR_RAMDISK)/vendor/firmware/tsp_stm/fts2ba61y_r0.bin \
+    $(LOCAL_PATH)/configs/firmware/vangogh_lite_unified.bin:$(TARGET_COPY_OUT_RECOVERY)/root/lib/firmware/sgpu/vangogh_lite_unified.bin \
+    $(LOCAL_PATH)/configs/firmware/vangogh_lite_unified.bin:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/firmware/sgpu/vangogh_lite_unified.bin
+
+# Init
+PRODUCT_PACKAGES += \
+    fstab.s5e9925 \
+    fstab.s5e9925.vendor_ramdisk \
+    init.recovery.s5e9925.rc
+
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+PRODUCT_PACKAGES += \
+    fastbootd
+
+# Platform
+TARGET_BOARD_PLATFORM := universal9925
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
